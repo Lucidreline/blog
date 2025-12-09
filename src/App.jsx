6cfbe0ec9ff3da08b76af0fe689c3464f31adbb6
Lucidreline <casta.ma502@gmail.com>
2025-12-08 17:37:58 -0800

    Got a quick router working.

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,14 @@
+import { Routes, Route, Link } from 'react-router-dom'
 import './App.scss'
+import Home from './pages/home/Home.component'
 
 function App() {
   return (
     <>
-      <h1>Testing</h1>
+      <h1>This is app here</h1>
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
     </>
   )
 }
---
+++ src/pages/home/Home.component.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './Home.styles.scss'
+
+const Home
+    = () => {
+        return (
+            <div>
+                <p>Home Page</p>
+            </div>
+        )
+    }
+
+export default Home
